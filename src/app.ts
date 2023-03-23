8ad37114f04484bcf0668f8cee8a024b074e359f
Christofer Assis <chris.f.assis18@gmail.com>
2023-03-22 23:51:09 -0300

    feat: add first route

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,26 @@
 import fastify from 'fastify';
+import { z } from 'zod';
+
+import { prisma } from '~/lib/prisma';
 
 export const app = fastify();
+
+app.post('/users', async (request, reply) => {
+	const registerBodySchema = z.object({
+		name: z.string(),
+		email: z.string().email(),
+		password: z.string().min(6),
+	});
+
+	const { email, name, password } = registerBodySchema.parse(request.body);
+
+	await prisma.user.create({
+		data: {
+			name,
+			email,
+			passwordHash: password,
+		},
+	});
+
+	return reply.status(201).send();
+});
