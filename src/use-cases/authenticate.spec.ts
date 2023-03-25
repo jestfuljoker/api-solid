d423c6e72f34abe9b973219ce1b8989d8e70e2df
Christofer Assis <chris.f.assis18@gmail.com>
2023-03-25 19:19:59 -0300

    test: ensure AuthenticateUseCase should not be able to authenticate with wrong password

--- src/use-cases/authenticate.spec.ts
+++ src/use-cases/authenticate.spec.ts
@@ -20,17 +20,27 @@ function makeSut(): SutTypes {
 	return { sut, inMemoryUsersRepository };
 }
 
+async function createUser(
+	inMemoryUsersRepository: InMemoryUsersRepository,
+	userData?: { email: string; password: string },
+): Promise<void> {
+	const email = userData?.email || faker.internet.email();
+	const password = userData?.password || faker.internet.password();
+
+	await inMemoryUsersRepository.create({
+		name: faker.name.fullName(),
+		email,
+		passwordHash: await hash(password, 6),
+	});
+}
+
 describe('Authenticate Use Case', () => {
 	it('should be able to authenticate', async () => {
 		const { sut, inMemoryUsersRepository } = makeSut();
 		const email = faker.internet.email();
 		const password = faker.internet.password();
 
-		await inMemoryUsersRepository.create({
-			name: faker.name.fullName(),
-			email,
-			passwordHash: await hash(password, 6),
-		});
+		await createUser(inMemoryUsersRepository, { email, password });
 
 		const { user } = await sut.handle({
 			email,
@@ -47,6 +57,19 @@ describe('Authenticate Use Case', () => {
 			sut.handle({
 				email: faker.internet.email(),
 				password: faker.internet.password(),
+			}),
+		).rejects.toBeInstanceOf(InvalidCredentialsError);
+	});
+
+	it('should not be able to authenticate with wrong password', async () => {
+		const { sut, inMemoryUsersRepository } = makeSut();
+
+		await createUser(inMemoryUsersRepository);
+
+		await expect(() =>
+			sut.handle({
+				email: faker.internet.email(),
+				password: 'wrong_password',
 			}),
 		).rejects.toBeInstanceOf(InvalidCredentialsError);
 	});
