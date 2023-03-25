d1bc76d46c63fa6d879ca776f5f99dc6df515c3f
Christofer Assis <chris.f.assis18@gmail.com>
2023-03-25 20:30:17 -0300

    feat: add authenticate route

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,7 +1,9 @@
 import type { FastifyInstance } from 'fastify';
 
-import { register } from './controllers';
+import { register, authenticate } from './controllers';
 
 export async function appRoutes(app: FastifyInstance): Promise<void> {
 	app.post('/users', register);
+
+	app.post('/sessions', authenticate);
 }
