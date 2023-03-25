164ba319abf4aa00a3a38c3c608f4589f6de4951
Christofer Assis <chris.f.assis18@gmail.com>
2023-03-25 16:07:52 -0300

    feat: add in memory repository

---
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -0,0 +1,27 @@
+import type { User, Prisma } from '@prisma/client';
+
+import type { UsersRepository } from '../users-repository';
+
+export class InMemoryUsersRepository implements UsersRepository {
+	users: User[] = [];
+
+	async create(data: Prisma.UserCreateInput): Promise<User> {
+		const user = {
+			id: 'user-1',
+			name: data.name,
+			email: data.email,
+			passwordHash: data.passwordHash,
+			createdAt: new Date(),
+		};
+
+		this.users.push(user);
+
+		return user;
+	}
+
+	async findByEmail(email: string): Promise<User | null> {
+		const user = this.users.find(user => user.email === email);
+
+		return user || null;
+	}
+}
---
+++ src/repositories/index.ts
@@ -0,0 +1,2 @@
+export * from './prisma/prisma-users-repository';
+export * from './in-memory/in-memory-users-repository';
