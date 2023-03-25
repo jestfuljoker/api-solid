710c3852a4f5a9231a2e328f0c079e99731719c6
Christofer Assis <chris.f.assis18@gmail.com>
2023-03-25 18:46:05 -0300

    feat: add authenticate use case

---
+++ src/use-cases/authenticate.ts
@@ -0,0 +1,40 @@
+import type { User } from '@prisma/client';
+import { compare } from 'bcryptjs';
+
+import type { UsersRepository } from '~/repositories/users-repository';
+
+import { InvalidCredentialsError } from './errors';
+
+interface AuthenticateUseCaseRequest {
+	email: string;
+	password: string;
+}
+
+interface AuthenticateUseCaseResponse {
+	user: User;
+}
+
+export class AuthenticateUseCase {
+	constructor(private usersRepository: UsersRepository) {}
+
+	async handle({
+		email,
+		password,
+	}: AuthenticateUseCaseRequest): Promise<AuthenticateUseCaseResponse> {
+		const user = await this.usersRepository.findByEmail(email);
+
+		if (!user) {
+			throw new InvalidCredentialsError();
+		}
+
+		const doesPasswordMatches = await compare(password, user.passwordHash);
+
+		if (!doesPasswordMatches) {
+			throw new InvalidCredentialsError();
+		}
+
+		return {
+			user,
+		};
+	}
+}
