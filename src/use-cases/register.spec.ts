54c7b803921ac8c80ccc490a7027a50a776db5e9
Christofer Assis <chris.f.assis18@gmail.com>
2023-03-25 19:07:59 -0300

    refactor: add faker to randomize user data

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,3 +1,4 @@
+import { faker } from '@faker-js/faker';
 import { compare } from 'bcryptjs';
 import { describe, expect, it } from 'vitest';
 
@@ -8,6 +9,28 @@ import { RegisterUseCase } from './register';
 
 interface SutTypes {
 	sut: RegisterUseCase;
+}
+
+interface User {
+	name: string;
+	email: string;
+	password: string;
+}
+
+interface CreateUserParams {
+	email?: string;
+	password?: string;
+}
+
+function createUser({
+	email = faker.internet.email(),
+	password = faker.internet.password(),
+}: CreateUserParams): User {
+	return {
+		name: faker.name.fullName(),
+		email,
+		password,
+	};
 }
 
 function makeSut(): SutTypes {
@@ -22,15 +45,12 @@ function makeSut(): SutTypes {
 describe('Register Use Case', () => {
 	it('should hash user password upon registration', async () => {
 		const { sut } = makeSut();
+		const password = faker.internet.password();
 
-		const { user } = await sut.handle({
-			name: 'Jon Doe',
-			email: 'jon.doe@mail.com',
-			password: '123456',
-		});
+		const { user } = await sut.handle(createUser({ password }));
 
 		const isPasswordCorrectlyHashed = await compare(
-			'123456',
+			password,
 			user.passwordHash,
 		);
 
@@ -39,31 +59,20 @@ describe('Register Use Case', () => {
 
 	it('should not be able to register with same email twice', async () => {
 		const { sut } = makeSut();
-		const email = 'jon.doe@mail.com';
+		const email = faker.internet.email();
+		const user = createUser({ email });
 
-		await sut.handle({
-			name: 'Jon Doe',
-			email,
-			password: '123456',
-		});
+		await sut.handle(user);
 
-		await expect(() =>
-			sut.handle({
-				name: 'Jon Doe',
-				email,
-				password: '123456',
-			}),
-		).rejects.toBeInstanceOf(UserAlreadyExistsError);
+		await expect(() => sut.handle(user)).rejects.toBeInstanceOf(
+			UserAlreadyExistsError,
+		);
 	});
 
 	it('should be able to register ', async () => {
 		const { sut } = makeSut();
 
-		const { user } = await sut.handle({
-			name: 'Jon Doe',
-			email: 'jon.doe@mail.com',
-			password: '123456',
-		});
+		const { user } = await sut.handle(createUser({}));
 
 		expect(user.id).toEqual(expect.any(String));
 	});
