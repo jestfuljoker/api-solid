98d9797404043e4574575be96fa2585f78b41d76
Christofer Assis <chris.f.assis18@gmail.com>
2023-03-28 21:41:39 -0300

    feat: implement create method in in memory gym repository

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,4 +1,6 @@
-import type { Gym } from '@prisma/client';
+import type { Prisma, Gym } from '@prisma/client';
+import { Decimal } from '@prisma/client/runtime/library';
+import { randomUUID } from 'crypto';
 
 import type { GymsRepository } from '../gyms-repository';
 
@@ -9,5 +11,20 @@ export class InMemoryGymsRepository implements GymsRepository {
 		const gym = this.gyms.find(gym => gym.id === id);
 
 		return gym || null;
+	}
+
+	async create(data: Prisma.GymCreateInput): Promise<Gym> {
+		const gym = {
+			id: data.id ?? randomUUID(),
+			title: data.title,
+			description: data.description ?? null,
+			phone: data.phone ?? null,
+			latitude: new Decimal(data.latitude.toString()),
+			longitude: new Decimal(data.longitude.toString()),
+		};
+
+		this.gyms.push(gym);
+
+		return gym;
 	}
 }
