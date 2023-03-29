78b3c76946043bb44b037304d811c664e2a16fde
Christofer Assis <chris.f.assis18@gmail.com>
2023-03-28 21:41:05 -0300

    feat: add new method to GymsRepository

--- src/repositories/gyms-repository.ts
+++ src/repositories/gyms-repository.ts
@@ -1,5 +1,6 @@
-import type { Gym } from '@prisma/client';
+import type { Gym, Prisma } from '@prisma/client';
 
 export interface GymsRepository {
 	findById(id: string): Promise<Gym | null>;
+	create(data: Prisma.GymCreateInput): Promise<Gym>;
 }
