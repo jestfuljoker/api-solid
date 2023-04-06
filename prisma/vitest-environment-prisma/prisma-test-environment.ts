9bbb21c3ab1fd4c1d0dc7b60ba42598d772bf725
Christofer Assis <chris.f.assis18@gmail.com>
2023-04-06 07:26:17 -0300

    feat: configure vitest environment to be used with prisma

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,13 +1,40 @@
-import type { Awaitable, Environment } from 'vitest';
+import { PrismaClient } from '@prisma/client';
+import 'dotenv/config';
+import { execSync } from 'node:child_process';
+import { randomUUID } from 'node:crypto';
+import type { Environment } from 'vitest';
+
+const prisma = new PrismaClient();
+
+function generateDatabaseURL(schema: string): string {
+	if (!process.env.DATABASE_URL) {
+		throw new Error('Please provide a valid DATABASE_URL environment variable');
+	}
+
+	const url = new URL(process.env.DATABASE_URL);
+
+	url.searchParams.set('schema', schema);
+
+	return url.toString();
+}
 
 export default <Environment>{
 	name: 'prisma',
 	async setup() {
-		console.log('Setup');
+		const schema = randomUUID();
+		const databaseUrl = generateDatabaseURL(schema);
+
+		process.env.DATABASE_URL = databaseUrl;
+
+		execSync('pnpm exec prisma migrate deploy');
 
 		return {
-			teardown(): Awaitable<void> {
-				console.log('Teardown');
+			async teardown(): Promise<void> {
+				await prisma.$executeRawUnsafe(
+					`DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+				);
+
+				await prisma.$disconnect();
 			},
 		};
 	},
