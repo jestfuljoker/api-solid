4ff01ed005519f8c000c8fdc45a3c45214c38166
Christofer Assis <chris.f.assis18@gmail.com>
2023-04-06 19:05:06 -0300

    test: add register e2e test

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,5 +1,25 @@
-import { expect, test } from 'vitest';
+import { faker } from '@faker-js/faker';
+import request from 'supertest';
+import { afterAll, beforeAll, describe, expect, it } from 'vitest';
 
-test('ok', () => {
-	expect(1 + 2).toBe(3);
+import { app } from '~/app';
+
+describe('Register (E2E)', () => {
+	beforeAll(async () => {
+		await app.ready();
+	});
+
+	afterAll(async () => {
+		await app.close();
+	});
+
+	it('should be able to register', async () => {
+		const response = await request(app.server).post('/users').send({
+			name: faker.name.fullName(),
+			email: faker.internet.email(),
+			password: faker.internet.password(),
+		});
+
+		expect(response.statusCode).toBe(201);
+	});
 });
